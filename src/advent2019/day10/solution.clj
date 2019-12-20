1d0120865fa6e2425bd49c0017330a8d098ea796
Grant Peltier <grantpeltier93@gmail.com>
2019-12-20 16:09:12 -0600

    adding day10 solution

--- src/advent2019/day10/solution.clj
+++ src/advent2019/day10/solution.clj
@@ -1,25 +1,38 @@
 (ns advent2019.day10.solution
   (:require [clojure.string :as string]))
 
+(def t0 ".#..#\n.....\n#####\n....#\n...##")
 (def t1 "......#.#.\n#..#.#....\n..#######.\n.#.#.###..\n.#..#.....\n..#....#.#\n#..#....#.\n.##.#..###\n##...#..#.\n.#....####")
 
 (defn blocked? [start block end]
   (if (= (:x start) (:x end))
-    (and (= (:x start) (:x end))
-         (< (:y start) (:y block))
-         (> (:y end) (:y block)))
+    (or (and (= (:x start) (:x block))
+             (< (:y start) (:y block) (:y end)))
+        (and (= (:x start) (:x block))
+             (> (:y start) (:y block) (:y end))))
     (let [slope (/ (- (:y end) (:y start))
                    (- (:x end) (:x start)))
           y-int (- (:y start) (* slope (:x start)))]
-      (= (:y block)
-         (+ y-int (* slope (:x block)))))))
+      (or (and (= (:y block)
+                  (+ y-int (* slope (:x block))))
+               (< (:x start) (:x block) (:x end)))
+          (and (= (:y block)
+                  (+ y-int (* slope (:x block))))
+               (> (:x start) (:x block) (:x end)))))))
 
-(defn visible-asteroids [all-asts ast]
-  (reduce (fn [vis curr]
-            (when (every? false? (map #(blocked? ast % curr) vis))
-              (conj vis curr)))
-          []
-          all-asts))
+(defn visible-asteroids [all-asts ast-idx]
+  (let [part-vis (fn [rng]
+                   (reduce (fn [vis curr]
+                             (if (every? false? (map #(blocked? (nth all-asts ast-idx)
+                                                                %
+                                                                (nth all-asts curr))
+                                                     vis))
+                               (conj vis (nth all-asts curr))
+                               vis))
+                           []
+                           rng))]
+    (concat (part-vis (reverse (range 0 ast-idx)))
+            (part-vis (range (inc ast-idx) (count all-asts))))))
 
 (defn part1 [data]
   (let [ast-pts (-> data
@@ -32,8 +45,71 @@
                                    (hash-map :x x :y y)))
                                row)))
                          flatten
-                         (remove nil?)))]
-    (reduce max (map #(count (visible-asteroids ast-pts %)) ast-pts))))
+                         (remove nil?)
+                         vec))]
+    (mapv #(count (visible-asteroids ast-pts %))
+          (range (count ast-pts)))
+    ;(reduce max (map #(count (visible-asteroids ast-pts %))
+    ;                 (range (count ast-pts))))
+    ))
+
+(defn dist [p1 p2]
+  (Math/sqrt (+ (Math/pow (- (:x p2) (:x p1)) 2)
+                (Math/pow (- (:y p2) (:y p1)) 2))))
+
+(defn angle [p1 p2]
+  (cond
+    (= (:x p1) (:x p2))
+    (if (> (:y p1) (:y p2))
+      0
+      Math/PI)
+
+    (= (:y p1) (:y p2))
+    (if (> (:x p2) (:x p1))
+      (/ Math/PI 2)
+      (* 3 (/ Math/PI 2)))
+
+    :else
+    (let [p3 {:x (:x p1) :y (:y p2)}
+          base-angle (Math/atan (/ (dist p3 p2)
+                                   (dist p1 p3)))]
+      (cond
+        (and (> (:y p2) (:y p1))                            ;; Q2
+             (> (:x p2) (:x p1)))
+        (+ (- (/ Math/PI 2) base-angle)
+           (/ Math/PI 2))
+
+        (and (> (:y p2) (:y p1))                            ;; Q3
+             (< (:x p2) (:x p1)))
+        (+ base-angle Math/PI)
+
+        (and (< (:y p2) (:y p1))                            ;; Q4
+             (< (:x p2) (:x p1)))
+        (+ (- (/ Math/PI 2) base-angle)
+           (* 3 (/ Math/PI 2)))
+
+        :else base-angle))))
 
 (defn part2 [data]
-  )
+  (let [ast-pts (-> data
+                    string/split-lines
+                    (->> (map-indexed
+                           (fn [y row]
+                             (map-indexed
+                               (fn [x char]
+                                 (when (= char \#)
+                                   (hash-map :x x :y y)))
+                               row)))
+                         flatten
+                         (remove nil?)
+                         vec))
+        all-viz (mapv #(visible-asteroids ast-pts %)
+                      (range (count ast-pts)))
+        viz-counts (mapv count all-viz)
+        max-count (reduce max viz-counts)
+        root (nth ast-pts (.indexOf viz-counts max-count))
+        vis-pts (nth all-viz (.indexOf viz-counts max-count))]
+    (-> vis-pts
+        (->> (sort-by (partial angle root)))
+        vec
+        (nth 199))))
